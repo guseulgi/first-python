2c7ee3e3b178a200e5f51bfb3ce0d1990a10ffdb
seulgi <guseul9405@gmail.com>
2023-06-14 15:38:33 +0900

    숫자 맞추기

--- main.py
+++ main.py
@@ -50,3 +50,17 @@ elif age >= 18 and age <= 35:
 else:
     print('Go ahead!')
 
+# 반복문 - 숫자 맞추기
+from random import randint
+playing = True
+pc_choice = randint(1, 50)
+
+while playing:
+  user_choice = int(input('Choose number'))
+  if user_choice == pc_choice:
+      print('You won!')
+      playing = False
+  elif user_choice > pc_choice:
+      print('Lower')
+  elif user_choice < pc_choice:
+      print('Higher')
