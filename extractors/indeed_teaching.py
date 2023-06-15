5206bbff6c8607e515b072a7bf8ec54a7f43416d
seulgi <guseul9405@gmail.com>
2023-06-16 00:01:29 +0900

    indeed 홈페이지 크롤링 함수 구현 완

--- extractors/indeed_teaching.py
+++ extractors/indeed_teaching.py
@@ -4,6 +4,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+total_pages = 5
+onepage_list_count = 10
+
 
 def get_page_count(keyword):
     options = Options()
@@ -16,43 +19,49 @@ def get_page_count(keyword):
     soup = BeautifulSoup(browser.page_source, 'html.parser')
     paginations = soup.find('nav', attrs={'aria-label': 'pagination'})
     if paginations == None:
-        return
+        return 1
     pages = paginations.select('div a')
-    print(len(pages))
+    count = len(pages)
+    if count >= total_pages:
+        return total_pages
+    else:
+        return count
 
 
 def extract_indeed_jobs(keyword):
-    options = Options()
-    options.add_experimental_option('detach', True)  # 브라우저 꺼짐 방지
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                               options=options)
-    base_url = f'https://kr.indeed.com/jobs?q={keyword}'
-    browser.get(base_url)
-
-    soup = BeautifulSoup(browser.page_source, 'html.parser')
-    job_list = soup.find('ul', class_='jobsearch-ResultsList')
-    jobs = job_list.find_all('li', recursive=False)
     results = []
-    for job in jobs:
-        zone = job.find('div', class_='mosaic-zone')
-        if zone == None:
-            anchor = job.select_one('h2 a')
-            link = anchor['href']
-            title = anchor['aria-label']
-            company = job.find('span', class_='companyName')
-            location = job.find('div', class_='companyLocation')
-            job_data = {
-                'link': f"https://kr.indeed.com/viewjob?{link}",
-                'company': company.string,
-                'location': location.string,
-                'position': title,
-            }
-            results.append(job_data)
-    for result in results:
-        print(result)
-        print('/////')
-    return results
+    pages = get_page_count(keyword)
+    for page in range(pages):
+        options = Options()
+        options.add_experimental_option('detach', True)  # 브라우저 꺼짐 방지
+        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                   options=options)
+        base_url = f'https://kr.indeed.com/jobs?q={keyword}&start={page * onepage_list_count}'
+        browser.get(base_url)
+
+        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        job_list = soup.find('ul', class_='jobsearch-ResultsList')
+        jobs = job_list.find_all('li', recursive=False)
+
+        for job in jobs:
+            zone = job.find('div', class_='mosaic-zone')
+            if zone == None:
+                anchor = job.select_one('h2 a')
+                link = anchor['href']
+                title = anchor['aria-label']
+                company = job.find('span', class_='companyName')
+                location = job.find('div', class_='companyLocation')
+                job_data = {
+                    'link': f"https://kr.indeed.com/viewjob?{link}",
+                    'company': company.string,
+                    'location': location.string,
+                    'position': title,
+                }
+                results.append(job_data)
+        for result in results:
+            print(result)
+            print('/////\n/////')
+        return results
 
 
-# extract_indeed_jobs('python')
-get_page_count('python')
+extract_indeed_jobs('python')
